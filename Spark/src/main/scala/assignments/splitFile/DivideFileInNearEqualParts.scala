bf54b1c9d38bd3f4a4579a67d6ea95ab69f8cf52
GAK <gauravk1994@gmail.com>
2022-12-15 12:18:51 +0530

    Added, code to split elements into near equal parts.

---
+++ Spark/src/main/scala/assignments/splitFile/DivideFileInNearEqualParts.scala
@@ -0,0 +1,37 @@
+package assignments.splitFile
+
+import assignments.splitFile.DivideFileInNearEqualParts.{elements, numberOfPartitions}
+
+import scala.collection.mutable.ArrayBuffer
+
+object DivideFileInNearEqualParts extends App {
+
+  def splitInNearEqualParts(elements:Int, numberOfPartitions:Int):ArrayBuffer[Int]={
+    var partitionsSizes = new ArrayBuffer[Int]
+
+    val rem = elements%numberOfPartitions
+    val div = elements/numberOfPartitions // will return int
+
+    if(elements > numberOfPartitions) {
+      if (rem == 0 & div != 0) {
+        partitionsSizes = ArrayBuffer.fill(numberOfPartitions)(div)
+      } else if (div == 0 & rem != 0) {
+        partitionsSizes = ArrayBuffer.fill(1)(rem)
+      } else if (rem != 0 & div != 0) {
+        partitionsSizes = ArrayBuffer.fill(numberOfPartitions)(div)
+        (0 until rem).foreach(idx => partitionsSizes(idx) = partitionsSizes(idx) + 1)
+      }
+    }else{
+      partitionsSizes = ArrayBuffer.fill(numberOfPartitions)(1)
+      (elements until numberOfPartitions).foreach(idx => partitionsSizes(idx) = 0)
+    }
+
+    partitionsSizes
+
+  }
+
+  val elements = 76
+  val numberOfPartitions = 10
+
+  println(splitInNearEqualParts(elements, numberOfPartitions))
+}
---
+++ Spark/src/main/scala/collection/Immutable.scala
@@ -0,0 +1,5 @@
+package collection
+
+object Immutable extends App {
+
+}
